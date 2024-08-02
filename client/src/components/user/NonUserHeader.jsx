9af158f1dd8e5e6046a0031abf7ab181bd33ac06
Farzaliyev <nureddin.fa@gmail.com>
2024-08-02 12:43:55 +0400

    user page
    small fix

--- client/src/components/user/NonUserHeader.jsx
+++ client/src/components/user/NonUserHeader.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom'
 function NonUserHeader() {
   return (
     <div className='bg-second-bg py-4 px-8 lg:px-16 flex justify-between items-center flex-col sm:flex-row'>
-        <h1 className='text-white font-bold font-display mb-3 sm:mb-0 text-big-font'>TechQuiz</h1>
+        <h1 className='text-white font-bold font-display mb-3 sm:mb-0 text-big-font'>
+          <Link to='/'>TechQuiz</Link>
+        </h1>
         <div className='h-min text-white text-med-font sm:text-big-font bg-main-accent hover:bg-second-accent transition-all px-5 py-2 rounded'>
             <Link to='/'>Login or Register</Link>
         </div>
