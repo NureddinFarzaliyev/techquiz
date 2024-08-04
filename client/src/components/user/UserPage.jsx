785dd4891e0f54698ce5322431e444b2b033451f
Farzaliyev <nureddin.fa@gmail.com>
2024-08-04 10:16:08 +0400

    progressbar

--- client/src/components/user/UserPage.jsx
+++ client/src/components/user/UserPage.jsx
@@ -4,7 +4,8 @@ import { useParams } from 'react-router-dom'
 import { fetchUserData } from '../Utils';
 import NonUserHeader from './NonUserHeader';
 import { Level } from '../Utils';
-import ProgressBar from './ProgressBar';
+import './user.css'
+import ProgressBarComponent from './ProgressBarComponent';
 
 function UserPage() {
 
@@ -22,15 +23,16 @@ function UserPage() {
         setUserLevel(Level(userData.points))
     }, [userData])
 
+    console.log(userLevel)
     return (
         <>
         { localStorage.getItem('userId') ? <Header/> : <NonUserHeader />}  
-        <section className='absolute left-[50%] top-[52.5%] translate-x-[-50%] translate-y-[-50%]'>
+        <section className='absolute left-[50%] top-[55%] translate-x-[-50%] translate-y-[-50%]'>
             <div className='flex flex-col items-center'>
                 <img src={imageUrl} className={`${userLevel.label}Border ${userLevel.label}Shadow border-4 w-44 h-44 sm:w-72 sm:h-72 rounded-full object-cover`} />
-                <h1 className="text-white text-xbig-font drop-shadow-md mt-4">{userData.username}</h1>
-                <p className='text-big-font text-second-text mt-[-20px]'>{`${userLevel.label} (${userData.points})`}</p>
-                <ProgressBar data={userLevel} />
+                <h1 className="text-white text-xbig-font drop-shadow-md mt-7">{userData.username}</h1>
+                <p className='text-big-font text-second-text mt-[-20px]'>{`${userLevel.label}`}</p>
+                <ProgressBarComponent data={userLevel} />
             </div>
         </section>
         </>
